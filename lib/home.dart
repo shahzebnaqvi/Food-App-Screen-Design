8a89ad3813667bae572cff77ccdcd2a4374e8919
shahzebnaqvi <shahzeb.naqvi12@gmail.com>
2021-10-28 02:37:59 +0500

    first commit

--- lib/home.dart
+++ lib/home.dart
@@ -36,21 +36,21 @@ class Home extends StatelessWidget {
       ),
       body: Container(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(
+              height: 20,
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Stack(
+                    alignment: Alignment.centerRight,
+                    children: [circlecontainer(), Icon(Icons.arrow_back)]),
                 Container(
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://www.w3schools.com/howto/img_avatar.png"))),
-                ),
-                Container(
-                  width: 180,
-                  height: 180,
+                  margin: EdgeInsets.all(5),
+                  width: 140,
+                  height: 140,
                   decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       border: Border.all(color: Colors.blue, width: 5),
@@ -58,20 +58,59 @@ class Home extends StatelessWidget {
                           image: NetworkImage(
                               "https://www.w3schools.com/howto/img_avatar.png"))),
                 ),
-                Container(
-                  width: 70,
-                  height: 70,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              "https://www.w3schools.com/howto/img_avatar.png"))),
-                )
+                Stack(
+                    alignment: Alignment.centerLeft,
+                    children: [circlecontainer(), Icon(Icons.arrow_back_sharp)])
               ],
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ListTile(
+              title: Text("Orignal Fried Shrimp"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Shrimp Category"),
+                  Row(
+                    children: [
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                      Icon(Icons.star),
+                      Icon(Icons.star)
+                    ],
+                  )
+                ],
+              ),
+              trailing: Text(
+                "6.9",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pellentesque, velit ac vestibulum finibus, velit odio pharetra sapien, non varius risus enim ac ex. Integer"),
+            Text("See more")
           ],
         ),
       ),
     );
   }
+}
+
+Widget circlecontainer() {
+  return Container(
+    margin: EdgeInsets.all(1),
+    width: 70,
+    height: 70,
+    decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: Colors.blue, width: 2),
+        image: DecorationImage(
+            image: NetworkImage(
+                "https://www.w3schools.com/howto/img_avatar.png"))),
+  );
 }
